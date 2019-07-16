280629b934aad78b8328a9e7da463a87d49593aa
wineok1234 <wineok@naver.com>
2019-07-16 16:45:19 +0900

    관리자 정보안의 버튼들 링크 연결

--- app/src/main/java/com/example/gurufinalproject/activity/administrator/CheckCommunalPropertyActivity.java
+++ app/src/main/java/com/example/gurufinalproject/activity/administrator/CheckCommunalPropertyActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gurufinalproject.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CheckCommunalPropertyActivity extends AppCompatActivity {
 
@@ -12,5 +14,14 @@ public class CheckCommunalPropertyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_communal_property);
+
+        findViewById(R.id.btnBack1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        AdministratorMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/gurufinalproject/activity/administrator/CheckLostArticleActivity.java
+++ app/src/main/java/com/example/gurufinalproject/activity/administrator/CheckLostArticleActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gurufinalproject.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CheckLostArticleActivity extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class CheckLostArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_lost_article);
+
+        findViewById(R.id.btnBack2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        AdministratorMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
