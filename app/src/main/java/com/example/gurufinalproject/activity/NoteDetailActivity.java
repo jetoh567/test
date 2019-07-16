a9228382abba27e2ba042a27351d6f2ece6ff2dc
wineok1234 <wineok@naver.com>
2019-07-16 11:45:24 +0900

    글쓰기 페이지, 글 상세보기 페이지 xml 작성

---
+++ app/src/main/java/com/example/gurufinalproject/activity/NoteDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.gurufinalproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gurufinalproject.R;
+
+public class NoteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_detail);
+    }
+}
