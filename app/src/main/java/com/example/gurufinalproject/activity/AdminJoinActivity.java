9132b34ec1b4531dbf72b7edac9031e2f0e19df4
Karen <1yw@naver.com>
2019-07-16 11:06:46 +0900

    2일차 소스 작성중 커밋

--- app/src/main/java/com/example/gurufinalproject/activity/AdminJoinActivity.java
+++ app/src/main/java/com/example/gurufinalproject/activity/AdminJoinActivity.java
@@ -5,23 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gurufinalproject.R;
 import com.example.gurufinalproject.bean.MemberBean;
+import com.example.gurufinalproject.db.FileDB;
 
-public class AdminJoinActivity extends AppCompatActivity {
+public class AdminJoinActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private String userid;
     private EditText edtName, edtPhone;
+    private long userNum ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_join);
 
+        Spinner department = findViewById(R.id.department);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.departments,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        department.setAdapter(adapter);
+        department.setOnItemSelectedListener(this);
         userid = getIntent().getStringExtra("AdminID");
         edtName = findViewById(R.id.edtName);
         edtPhone = findViewById(R.id.edtPhoneNum);
@@ -35,7 +47,6 @@ public class AdminJoinActivity extends AppCompatActivity {
                 joinProcess();
             }
         });
-
 
     }
 
@@ -58,6 +69,19 @@ public class AdminJoinActivity extends AppCompatActivity {
             return;
         }
         memberBean.phoneNum = phone;
+        memberBean.userNum = userNum;
 
+        //관리자 회원가입 후 추가
+        FileDB.addAdmin(this,memberBean);
+        Toast.makeText(this,"회원가입이 완료되었습니다.",Toast.LENGTH_SHORT).show();
+        finish();
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+       userNum = adapterView.getItemIdAtPosition(i);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) { }
 }
--- app/src/main/java/com/example/gurufinalproject/bean/MemberBean.java
+++ app/src/main/java/com/example/gurufinalproject/bean/MemberBean.java
@@ -3,6 +3,6 @@ package com.example.gurufinalproject.bean;
 public class MemberBean {
     public String name;
     public String userid;
-    public String userNum;
+    public long userNum;
     public String phoneNum;
 }
