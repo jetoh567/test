fa45a10221c28e075f464c1704eaf79f39cd6635
Karen <1yw@naver.com>
2019-07-19 14:11:28 +0900

    5일차

--- app/src/main/java/com/example/gurufinalproject/activity/administrator/AdminNoticeActivity.java
+++ app/src/main/java/com/example/gurufinalproject/activity/administrator/AdminNoticeActivity.java
@@ -99,7 +99,7 @@ public class AdminNoticeActivity extends AppCompatActivity {
 
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     NoticeBean notice = snapshot.getValue(NoticeBean.class);
-                   noticeList.add(notice);
+                   noticeList.add(0,notice);
                 }
 
                 //바뀌는 데이터로 refresh 한다.
@@ -115,64 +115,5 @@ public class AdminNoticeActivity extends AppCompatActivity {
         });
 
     }// end onResume
-
-    public class NoticeAdapter extends BaseAdapter {
-
-        private Context mContext;
-        private List<NoticeBean> mNoticeList;
-
-
-        public NoticeAdapter(Context context, List<NoticeBean> noticeList) {
-            mContext = context;
-            mNoticeList = noticeList;
-        }
-
-        @Override
-        public int getCount() {
-            return mNoticeList.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return mNoticeList.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            final LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            view = inflater.inflate(R.layout.notice_form,null);
-
-            TextView title, detail, writer, date;
-
-            title = view.findViewById(R.id.NoticeTitle);
-            detail = view.findViewById(R.id.NoticeDetail);
-            writer = view.findViewById(R.id.NoticeWriter);
-            date = view.findViewById(R.id.NoticeDate);
-
-            final NoticeBean noticeBean = mNoticeList.get(i);
-
-            title.setText(noticeBean.noticeTitle);
-            detail.setText(noticeBean.noticeDetail);
-            writer.setText("작성자 : "+noticeBean.noticeWriter);
-            date.setText("작성 날짜 : "+noticeBean.regDate);
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent i = new Intent(getBaseContext(),NoticeDetailActivity.class);
-                    i.putExtra("notice",noticeBean);
-                    startActivity(i);
-                }
-            });
-
-            return view;
-        }
-    }
 
 }
--- app/src/main/java/com/example/gurufinalproject/fragment/FragmentMemberAlert.java
+++ app/src/main/java/com/example/gurufinalproject/fragment/FragmentMemberAlert.java
@@ -7,25 +7,64 @@ import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.gurufinalproject.R;
+import com.example.gurufinalproject.activity.administrator.AdminNoticeActivity;
+import com.example.gurufinalproject.activity.administrator.NoticeAdapter;
 import com.example.gurufinalproject.bean.NoteBean;
+import com.example.gurufinalproject.bean.NoticeBean;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentMemberAlert extends Fragment {
-    private ListView listView;
-    private ListAdapter adapter;
-    public List<NoteBean> noteList = new ArrayList<>();
+    private FirebaseDatabase mFirebaseDB = FirebaseDatabase.getInstance();
+    private ListView mLstNotice;
+    public NoticeAdapter mAdapter;
+    public List<NoticeBean> noticeList = new ArrayList<>();
 
     public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_member_alert, container, false);
 
+        mLstNotice = view.findViewById(R.id.lstAlertView);
+        mAdapter = new NoticeAdapter(getContext(),noticeList);
+        mLstNotice.setAdapter(mAdapter);
+
         return view;
     }
 
+    public void onResume() {
+        super.onResume();
+        mFirebaseDB.getReference().child("Notice").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //데이터 받아와서 리스트에 선언
+                noticeList.clear();
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    NoticeBean notice = snapshot.getValue(NoticeBean.class);
+                    noticeList.add(0,notice);
+                }
+
+                //바뀌는 데이터로 refresh 한다.
+                if(mAdapter!=null){
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+}
 }
